1d8ea02c36c6e63eff50ef741e41f4bfb77d2b5d
Dejan <dichoski.d@hotmail.com>
2023-05-06 19:24:41 +0200

    Updates to plotting functions and RBCGD

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ from matplotlib import pyplot as plt
 import pandas as pd
 from time import process_time
 from tqdm.auto import tqdm
+import math
 
 def data_generation(n_points=1000, subset_size=0.02):
     points_dict = {}
 
     for i in range(int(n_points / 2)):
-        # points_dict[(3 + randn(), 3 + randn())] = 1
-        # points_dict[(4.5 + randn(), 4.5 + randn())] = -1
-        points_dict[(3 + randn(), randn())] = 1
-        points_dict[(1.5 * randn(), 4 + randn())] = -1
+        points_dict[(3 + randn(), 3 + randn())] = 1
+        points_dict[(45 + randn(), 45 + randn())] = -1
+        #points_dict[(3 + randn(), randn())] = 1
+        #points_dict[(1.5 * randn(), 4 + randn())] = -1
 
     points_list = [key for key in points_dict.keys()]
     labels_list = [value for value in points_dict.values()]
@@ -42,12 +43,14 @@ def setup_points(n_points=1000, subset_size=0.02):
     print("Computing similarity matrix...")
     dict_weights_similarity = calculate_similarity_weights({**dict_labeled, **dict_nonlabeled})
     print("Similarity matrix constructed.")
-
     return dict_nonlabeled, dict_labeled, dict_test, dict_weights_similarity
 
-
-def similarity(x_1, x_2):
     return 1 / (np.sqrt((x_1[0] - x_2[0]) ** 2 + (x_1[1] - x_2[1]) ** 2))
+
+def similarity(x1, x2): # Euclidean
+    dist = [(a - b)**2 for a, b in zip(x1, x2)]
+    dist = math.sqrt(sum(dist))
+    return dist
 
 def calculate_similarity_weights(dict_points):
     dict_similarity_weights = {}
@@ -56,7 +59,6 @@ def calculate_similarity_weights(dict_points):
             if point1 != point2:
                 dict_similarity_weights[(point1, point2)] = similarity(point1, point2)
     return dict_similarity_weights
-
 
 def plotting(points, labels, non_labeled_points={}, nl=False):
     if nl:
@@ -72,6 +74,12 @@ def plotting(points, labels, non_labeled_points={}, nl=False):
         group.plot(ax=axis, kind='scatter', x='x', y='y', label=key, color=colors[key])
     plt.show()
 
+def calculate_accuracy(test_dict, pred_dict):
+    correct = 0
+    for key in test_dict.keys():
+        if pred_dict[key] == test_dict[key]:
+            correct += 1
+    return correct / len(test_dict)
 
 def calculate_loss(dict_nonlabeled, dict_labeled, dict_weights):
     # w:
@@ -80,16 +88,54 @@ def calculate_loss(dict_nonlabeled, dict_labeled, dict_weights):
     second_term_gradient = [(dict_weights[x_j, x_i] * (y_j - y_i) * (y_j - y_i)) for x_j, y_j in dict_nonlabeled.items() for x_i, y_i in dict_nonlabeled.items() if x_i != x_j]
     return sum(first_term_gradient) + 0.5 * sum(second_term_gradient)
 
-def gradient_descent(dict_unlabeled, dict_labeled, dict_test, dict_similarity_weights, alpha= 1e-4, grad_thres=0.005, to_plot=True, rbcgd=False):
-    stop_cond = False
-    loss_list, cpu_time = [], []
-    max_iter = 1000
+# Partial gradient (returns a scalar)
+def gradient_yj(x_j, y_j, dict_unlabeled, dict_labeled, dict_weights):
+    first_term_gradient  = [(dict_weights[x_j, x_i] * (y_j - y_i)) for x_i, y_i in dict_labeled.items()   if x_i != x_j]
+    second_term_gradient = [(dict_weights[x_j, x_i] * (y_j - y_i)) for x_i, y_i in dict_unlabeled.items() if x_i != x_j]
+    return 2 * (sum(first_term_gradient) + sum(second_term_gradient))
 
-    for iteration in tqdm(range(max_iter)):
+# Total/Full gradient (returns a vector)
+def gradient_total(dict_unlabeled, dict_labeled, dict_weights):
+    grad_list = []
+    for x_j, y_j in dict_unlabeled.items():
+        first_term_gradient = [(dict_weights[x_j, x_i] * (y_j - y_i)) for x_i, y_i in dict_labeled.items() if x_i != x_j]
+        second_term_gradient = [(dict_weights[x_j, x_i] * (y_j - y_i)) for x_i, y_i in dict_unlabeled.items() if x_i != x_j]
+        grad_list.append(2 * (sum(first_term_gradient) + sum(second_term_gradient)))
+    return np.array(grad_list)
+
+def rbcgd(dict_unlabeled, dict_labeled, dict_similarity_weights,
+          iterations=1000, alpha=1e-2, grad_thres=1e-3):
+    loss_list, cpu_time_list = [], []
+    num_unlabelled = len(dict_unlabeled)
+    U = np.zeros(num_unlabelled)
+    print("Minimizing objective function:")
+    for iteration in tqdm(range(iterations)):
         t_before = process_time()
         loss_list.append(calculate_loss(dict_unlabeled, dict_labeled, dict_similarity_weights))
-        for x_j, y_j in dict_unlabeled.items():
-            grad = gradient_yj(x_j, y_j, dict_unlabeled, dict_labeled, dict_similarity_weights, rbcgd)
+        grad = gradient_total(dict_unlabeled, dict_labeled, dict_similarity_weights)
+        random_pos = int(np.random.choice(num_unlabelled, 1))
+        U[random_pos] = 1.0
+        update_value = - alpha * U * grad
+        array_unlabeled = np.array(list(dict_unlabeled.values()))
+        array_unlabeled += update_value
+        dict_unlabeled = dict(zip(dict_unlabeled.keys(), array_unlabeled))
+        t_after = process_time()
+        cpu_time_list.append(t_after - t_before)
+        if np.linalg.norm(grad) < grad_thres:
+            break
+    return loss_list, cpu_time_list
+
+def gradient_descent(dict_unlabeled, dict_labeled, dict_similarity_weights,
+                     iterations=1000, alpha=1e-2, grad_thres=1e-3):
+    stop_cond = False
+    loss_list, cpu_time_list = [], []
+    print("Minimizing objective function:")
+    for iteration in range(iterations):
+        print("Iteration:", iteration)
+        t_before = process_time()
+        loss_list.append(calculate_loss(dict_unlabeled, dict_labeled, dict_similarity_weights))
+        for x_j, y_j in tqdm(dict_unlabeled.items()):
+            grad = gradient_yj(x_j, y_j, dict_unlabeled, dict_labeled, dict_similarity_weights)
             if abs(grad) > grad_thres:
                 dict_unlabeled[x_j] = y_j - alpha * grad
             else:
@@ -97,59 +143,64 @@ def gradient_descent(dict_unlabeled, dict_labeled, dict_test, dict_similarity_we
                 print(f'Current gradient value: {abs(grad)} < {grad_thres}.\nStopping at iteration {iteration}.')
                 break
         t_after = process_time()
-        cpu_time.append(t_after - t_before)
+        cpu_time_list.append(t_after - t_before)
         if stop_cond:
             break
+    return loss_list, cpu_time_list
 
-    for x_j, y_j in dict_unlabeled.items():
-        if y_j >= 0:
-            dict_unlabeled[x_j] = 1
-        else:
-            dict_unlabeled[x_j] = -1
-
-    print(f"Unlabeled points have been classified with an accuracy of {calculate_accuracy(dict_test, dict_unlabeled)}.")
-
-    #### Plotting part ##############
-    if to_plot == True:
-        all_points = {**dict_labeled, **dict_unlabeled}
-        all_points_list = [[key[0], key[1]] for key in all_points.keys()]
-        all_points_list = np.array(all_points_list)
-        plotting(all_points_list, all_points.values())
-
-    return loss_list, cpu_time
-
-
-def gradient_yj(x_j, y_j, dict_unlabeled, dict_labeled, dict_weights, rbcgd=False):
-    if rbcgd:
-        feature = int(np.random.choice(2, 1))
-        first_term_gradient = [(1 / abs(x_j[feature] - x_i[feature]) * (y_j - y_i)) for x_i, y_i in dict_labeled.items() if x_i[feature] != x_j[feature]]
-        second_term_gradient = [(1 / abs(x_j[feature] - x_i[feature]) * (y_j - y_i)) for x_i, y_i in dict_unlabeled.items() if x_i[feature] != x_j[feature]]
+def plot_loss(loss_list, algorithm, acc, alpha, grad_thres, cpu_time_list=[]):
+    num_iterations = len(loss_list)
+    fig, ax = plt.subplots()
+    plt.grid(alpha=0.3)
+    ax.set_title('{}\nAccuracy: {:.2f}%\nLearning Rate: {}\nGradient threshold: {}\nIterations: {}'.format(algorithm, acc * 100, alpha, grad_thres, num_iterations))
+    ax.set_ylabel("Loss")
+    if cpu_time_list:
+        plt.plot(np.cumsum(cpu_time_list), loss_list, color='blue', marker='o', markerfacecolor='r')
+        ax.set_xlabel("CPU time")
+        filename = 'GD_acc{:.2f}_alpha{}_cpu_time{}.png'.format(acc, alpha, sum(loss_list))
     else:
-        first_term_gradient = [(dict_weights[x_j, x_i] * (y_j - y_i)) for x_i, y_i in dict_labeled.items() if x_i != x_j]
-        second_term_gradient = [(dict_weights[x_j, x_i] * (y_j - y_i)) for x_i, y_i in dict_unlabeled.items() if x_i != x_j]
-    return 2 * (sum(first_term_gradient) + sum(second_term_gradient))
+        plt.plot(loss_list, color='blue', marker='o', markerfacecolor='r')
+        ax.set_xlabel("Number of iterations")
+        filename = 'GD_acc{:.2f}_alpha{}_num_iter{}.png'.format(acc, alpha, num_iterations)
 
-def calculate_accuracy(test_dict, pred_dict):
-    correct = 0
-    for key in test_dict.keys():
-        if pred_dict[key] == test_dict[key]:
-            correct += 1
-    return correct / len(test_dict)
+    plt.savefig(filename)  # save the graph as an image with the parameters in the filename
+    plt.show()
+
+    # save the parameters and accuracy to a text file with the same filename as the image
+    with open(filename.replace('.png', '.txt'), 'w') as f:
+        f.write('Learning Rate: {}\n'.format(alpha))
+        f.write('Iterations: {}\n'.format(num_iterations))
+        f.write('Accuracy: {:.2f}%\n'.format(acc * 100))
 
 
 if __name__ == '__main__':
     print("Start:")
-    dict_unlabeled, dict_labeled, dict_test, dict_similarity_weights = setup_points(n_points=6000, subset_size=0.01)
+    alpha = 1e-6
+    grad_thres = 0.0005
+    iterations = 10
+    algorithms = ['Gradient Descent', 'RBCGD']
+    algorithm = algorithms[1]
+    dict_unlabeled, dict_labeled, dict_test, dict_similarity_weights = setup_points(n_points=1000, subset_size=0.01)
+    loss_list, cpu_time_list = rbcgd(dict_unlabeled, dict_labeled, dict_similarity_weights,
+                                     iterations=iterations,
+                                     alpha=alpha,
+                                     grad_thres=grad_thres)
 
-    func_values_gdo, cpu_time_gdo = gradient_descent(dict_unlabeled, dict_labeled, dict_test, dict_similarity_weights)
-                                                        # alpha = 1e-3, grad_thres = 0.005, to_plot = True, rbcgd=False)
+    dict_unlabeled = {x_j: 1 if y_j >= 0 else -1 for x_j, y_j in dict_unlabeled.items()}
+    all_points = {**dict_labeled, **dict_unlabeled}
+    all_points_list = [[key[0], key[1]] for key in all_points.keys()]
+    all_points_list = np.array(all_points_list)
+    plotting(all_points_list, all_points.values())
 
-    # Replace the following code with a function
-    fig, ax = plt.subplots()
-    plt.grid(alpha = 0.2)
-    plt.plot(func_values_gdo, color='blue', marker = 'o', markerfacecolor = 'r')
-    ax.set_ylabel("Loss")
-    ax.set_xlabel("Number of iterations")
-    plt.show()
+    acc = calculate_accuracy(dict_test, dict_unlabeled)
+    # Accuracy/Loss vs Iterations
+    plot_loss(loss_list, algorithm, acc, alpha, grad_thres)
+    # Accuracy/Loss vs CPU Time
+    plot_loss(loss_list, algorithm, acc, alpha, grad_thres, cpu_time_list=cpu_time_list)
     print("Finished.")
+
+
+
+
+
 
